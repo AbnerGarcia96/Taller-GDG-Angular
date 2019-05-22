c5fe911b8dd4288a71611019f6e803d804df846e
Abner García <abnerjosgarfer@yahoo.com>
2019-05-21 21:15:39 -0600

    Cambio de SASS a SCSS y agregado el navbar

--- src/app/components/registros/registros.component.ts
+++ src/app/components/registros/registros.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registros',
   templateUrl: './registros.component.html',
-  styleUrls: ['./registros.component.sass']
+  styleUrls: ['./registros.component.scss']
 })
 export class RegistrosComponent implements OnInit {
 
