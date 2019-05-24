c404bb14b7eea5a8472d14741301dd72a794fc89
Abner García <abnerjosgarfer@yahoo.com>
2019-05-23 20:35:37 -0600

    Código listo

--- src/app/components/registros/registros.component.ts
+++ src/app/components/registros/registros.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './registros.component.html',
   styleUrls: ['./registros.component.scss']
 })
-export class RegistrosComponent implements OnInit {
+export class RegistrosComponent {
+  registros: any;
+  registroNuevo: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.registros = [{
+      nombre: 'Abner',
+      edad: 22
+    }];
+    this.registroNuevo = {};
   }
 
+  agregarRegistro(){
+    this.registros.push(this.registroNuevo);
+  }
+
+  eliminarTodos(){
+    this.registros = [];
+  }
 }
